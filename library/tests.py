bd5d474525cee61679253a924656bdec6c397bdb
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-11-13 20:45:33 +0300

    release base test for book list

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+import json
+
+from django.test import TestCase, Client
+
+from .models import Book
+
 
 # Create your tests here.
+class BookTestCase(TestCase):
+    book1 = {
+        'id': 1,
+        'title': 'book1 title',
+        'year': 2020,
+        'genre': 'comedy',
+        'annotation': 'a-a-a-a-a',
+    }
+    book2 = {
+        'id': 2,
+        'title': 'book2 title',
+        'year': 1988,
+        'genre': 'tragedy',
+        'annotation': 'b-b-b-b-b',
+    }
+
+    def setUp(self):
+        book1 = Book.objects.create(**self.book1)
+        book2 = Book.objects.create(**self.book2)
+
+    def test_books_list(self):
+        client = Client()
+        response = client.get('/books/')
+        self.assertEqual(response.status_code, 200)
+
+
+
